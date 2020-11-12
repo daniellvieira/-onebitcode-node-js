7a30dfe8db783a78be577701b2f9277f6dbba4c3
Daniel Vieira <danielvieiraufc@gmail.com>
2020-11-12 11:34:43 -0300

    Respostas JSON

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
@@ -5,6 +6,10 @@ const port = 3000
 // Primeira rota com Express
 app.get('/', (req, res) => {
   res.send('<h1>TODO List</h1>')
+})
+
+app.get('/json', (req, res) => {
+  res.json({title: 'Tarefa X', done: true})
 })
 
 app.listen(port, () => {
