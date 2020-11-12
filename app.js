3ec38ab4a691743b2513993262c06000a334bc5a
Daniel Vieira <danielvieiraufc@gmail.com>
2020-11-12 12:00:22 -0300

    Concentrando rotas

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
-const { response } = require('express')
 const express = require('express')
+const checkListRouter = require('./src/routes/checklists')
 const app = express()
 const port = 3000
 
 // verifica se existe um JSON dentro da chamada web e disponibiliza
 // dentro do req.body
 app.use(express.json())
-
-// criar um middleware
-const log = (req, res, next) => {
-  console.log(req.body)
-  console.log(`Data: ${Date.now()}`)
-  next()
-}
-
-app.use(log)
-
-// Primeira rota com Express
-app.get('/', (req, res) => {
-  res.send('<h1>TODO List</h1>')
-})
-
-app.get('/json', (req, res) => {
-  console.log(req.body)
-  res.json({title: 'Tarefa X', done: true})
-})
+app.use('/checklists', checkListRouter)
 
 app.listen(port, () => {
   console.log(`Servidor foi iniciado em http://localhost:${port}`)
